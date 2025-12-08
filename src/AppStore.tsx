66e608cff4891c9beb5d1073000f4b4cab6c7fc3
jhaayush11 <jhanathayush@gmail.com>
2025-12-08 20:20:20 +0530

    Pipeline fixed

--- src/AppStore.tsx
+++ src/AppStore.tsx
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import JwTSliceReducer from "./Slices/JWTSlice"
-import UserSliceReducer from "./Slices/UserSlice"
+// import JwTSliceReducer from "./Slices/JwtSlice";
+// import { } from "./Slices/JwtSlice";
+import JwTSliceReducer from "./Slices/AuthSlice";
+import UserSliceReducer from "./Slices/UserSlice";
 import profileSliceReducer from "./Slices/ProfileSlice";
 
 const appStore = configureStore({
--- src/Slices/AuthSlice.tsx
+++ src/Slices/AuthSlice.tsx
@@ -2,16 +2,16 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const JwtSlice = createSlice({
   name: "JwtSlice",
-  initialState: localStorage.getItem('jwtToken')||'',
+  initialState: localStorage.getItem("jwtToken") || "",
   reducers: {
     addJWTToken: (state, action) => {
-      localStorage.setItem('jwtToken', action.payload);
+      localStorage.setItem("jwtToken", action.payload);
       state = action.payload;
       return state;
     },
     deleteJWTToken: (state) => {
       localStorage.removeItem("jwtToken");
-      state = '';
+      state = "";
       return state;
     },
   },
